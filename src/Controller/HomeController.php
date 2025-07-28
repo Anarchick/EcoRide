34439b0762c7caf5552714d3536050ac1b18f257
aeim <aeim@hotmail.fr>
2025-07-28 09:58:34 +0200

    add static home, header, footer + css

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Form\TravelSearchType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -11,8 +12,11 @@ final class HomeController extends AbstractController
     #[Route('/', name: 'app_home')]
     public function index(): Response
     {
+        $searchForm = $this->createForm(TravelSearchType::class);
+        
         return $this->render('home/index.html.twig', [
             'controller_name' => 'HomeController',
+            'search_form' => $searchForm,
         ]);
     }
 }
