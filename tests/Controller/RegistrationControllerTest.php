5dba85b8f70edc981beeb80b6946540fdd0850d7
aeim <aeim@hotmail.fr>
2025-09-01 23:31:43 +0200

    update TestUtils to be instanciated

--- tests/Controller/RegistrationControllerTest.php
+++ tests/Controller/RegistrationControllerTest.php
@@ -3,7 +3,10 @@
 namespace App\Tests;
 
 use App\Repository\UserRepository;
-use Doctrine\ORM\EntityManager;
+use App\Tests\Utils\TestUtils;
+use App\Validator\Constraints\UniqueEmail;
+use Doctrine\Common\DataFixtures\Purger\ORMPurger;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\KernelBrowser;
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
@@ -11,40 +14,64 @@ class RegistrationControllerTest extends WebTestCase
 {
     private KernelBrowser $client;
     private UserRepository $userRepository;
+    private EntityManagerInterface $em;
 
     protected function setUp(): void
     {
         $this->client = static::createClient();
-
-        // Ensure we have a clean database
         $container = static::getContainer();
-
-        /** @var EntityManager $em */
-        $em = $container->get('doctrine')->getManager();
         $this->userRepository = $container->get(UserRepository::class);
+        $this->em = $container->get('doctrine')->getManager();
 
-        foreach ($this->userRepository->findAll() as $user) {
-            $em->remove($user);
-        }
-
-        $em->flush();
+        TestUtils::purgeDatabase($this->em);
     }
 
-    public function testRegister(): void
+    private function submitForm(string $email): void
+    {
+        $this->client->submitForm("S'inscrire", [
+            'registration[firstName]' => 'John',
+            'registration[lastName]' => 'Doe',
+            'registration[username]' => 'johndoe',
+            'registration[phone]' => '+33612345678',
+            'registration[email]' => $email,
+            'registration[plainPassword][first]' => '2]~4t.C6=pqN23',
+            'registration[plainPassword][second]' => '2]~4t.C6=pqN23',
+            'registration[agreeTerms]' => true,
+        ]);
+    }
+
+    public function testValidRegister(): void
     {
         // Register a new user
         $this->client->request('GET', '/register');
         self::assertResponseIsSuccessful();
-        self::assertPageTitleContains('Register');
+        self::assertPageTitleContains('Inscription');
 
-        $this->client->submitForm('Register', [
-            'registration_form[email]' => 'me@example.com',
-            'registration_form[plainPassword]' => 'password',
-            'registration_form[agreeTerms]' => true,
-        ]);
+        $uniqueEmail = uniqid() . '@test.com';
 
-        // Ensure the response redirects after submitting the form, the user exists, and is not verified
-        // self::assertResponseRedirects('/'); @TODO: set the appropriate path that the user is redirected to.
-        self::assertCount(1, $this->userRepository->findAll());
+        $this->submitForm($uniqueEmail);
+        $this->assertResponseRedirects('/login');
+
+        // Vérifier que l'utilisateur a bien été créé en base de données
+        $user = $this->userRepository->findOneByEmail($uniqueEmail);
+        $this->assertNotNull($user);
+        $this->assertEquals('+33 6 12 34 56 78', $user->getPhone());
+    }
+
+    public function testRegisterWithExistingEmail(): void
+    {
+        $this->assertCount(0, $this->userRepository->findAll(), 'No user should exist before registration');
+        $email = 'test@exemple.com';
+        $user = TestUtils::createUser(email: $email);
+        $this->em->persist($user);
+        $this->em->flush();
+
+        $this->client->request('GET', '/register');
+        self::assertResponseIsSuccessful();
+
+        $this->submitForm($email);
+        $this->assertSelectorExists('#registration_email.is-invalid');
+        $this->assertSelectorTextContains('.invalid-feedback', UniqueEmail::DEFAULT_MESSAGE);
+        $this->assertCount(1, $this->userRepository->findAll(), 'Only one user should exist with this email');
     }
 }
--- tests/Utils/TestUtils.php
+++ tests/Utils/TestUtils.php
@@ -5,10 +5,18 @@ use App\Entity\User;
 use App\Repository\UserRepository;
 use Doctrine\Common\DataFixtures\Purger\ORMPurger;
 use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInterface;
 
 class TestUtils
 {
-    public static function createUser(
+
+    public function __construct(
+        private ContainerInterface $container
+    ) {}
+
+    public function createUser(
         string $firstName = 'John',
         string $lastName = 'Doe',
         string $username = 'johndoe',
@@ -17,6 +25,9 @@ class TestUtils
         string $password = '2]~4t.C6=pqN23'
     ): User
     {
+        /** @var UserPasswordHasherInterface $passwordHasher */
+        $passwordHasher = $this->container->get('security.user_password_hasher');
+
         $user = new User();
         $user->setFirstName($firstName)
             ->setLastName($lastName)
@@ -24,21 +35,33 @@ class TestUtils
             ->setEmail($email)
             ->setEmailHash(hash('sha256', $email))
             ->setPhone($phone)
-            ->setPassword($password);
+            ->setPassword($passwordHasher->hashPassword($user, $password));
 
         return $user;
     }
 
-    public static function purgeDatabase(EntityManagerInterface $em): void
+    public function getEntityManager(): EntityManagerInterface
     {
+        return $this->container->get('doctrine')->getManager();
+    }
+
+    public function getUserRepository(): UserRepository
+    {
+        return $this->getEntityManager()->getRepository(User::class);
+    }
+
+    public function getSecurityTokenStorage(): TokenStorageInterface
+    {
+        return $this->container->get('security.token_storage');
+    }
+
+    public function purgeDatabase(): void
+    {
+        $em = $this->getEntityManager();
         $purger = new ORMPurger($em);
 
         $purger->purge();
         $em->clear();
     }
 
-    public static function getUserRepository(EntityManagerInterface $em): UserRepository
-    {
-        return $em->getRepository(User::class);
-    }
 }
