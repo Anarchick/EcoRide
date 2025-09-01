bf333bea5f045413a1af6ed8dbc98de11b6c0c54
aeim <aeim@hotmail.fr>
2025-09-01 22:45:23 +0200

    Update UniqueEmail

--- src/Validator/Constraints/UniqueEmail.php
+++ src/Validator/Constraints/UniqueEmail.php
@@ -7,7 +7,19 @@ use Symfony\Component\Validator\Constraint;
 #[\Attribute]
 class UniqueEmail extends Constraint
 {
-    public string $message = 'Cet email est déjà utilisé.';
+    public const DEFAULT_MESSAGE = 'Cette adresse email est déjà utilisée.';
+
+    public string $message = self::DEFAULT_MESSAGE;
+
+    public function __construct(
+        ?string $message = null,
+        ?array $groups = null,
+        mixed $payload = null
+    ) {
+        parent::__construct([], $groups, $payload);
+        
+        $this->message = $message ?? self::DEFAULT_MESSAGE;
+    }
 
     public function getTargets(): string
     {
