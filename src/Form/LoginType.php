ff1417a450282f16a7186b6217e5e76cba88fa11
aeim <aeim@hotmail.fr>
2025-09-01 11:48:20 +0200

    Add Login system

--- src/Form/LoginType.php
+++ src/Form/LoginType.php
@@ -3,9 +3,10 @@
 namespace App\Form;
 
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
-use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -14,8 +15,11 @@ class LoginType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('email', TextType::class)
+            ->add('email', EmailType::class)
             ->add('password', PasswordType::class)
+            ->add('remember_me', CheckboxType::class, [
+                'required' => false,
+            ])
             ->add('submit', SubmitType::class)
         ;
     }
