99d472762014cf7a05b1d3f3f5254327671a21c8
aeim <aeim@hotmail.fr>
2025-09-07 20:24:13 +0200

    Update app_member route

--- src/Controller/MemberController.php
+++ src/Controller/MemberController.php
@@ -8,13 +8,13 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 // Faster regex UUID
-#[Route('/member/{slug}', name: 'app_member_', requirements: ['slug' => '[a-f0-9]{32}'])]
+#[Route('/member/{uuid32}', name: 'app_member_', requirements: ['uuid32' => '[a-f0-9]{32}'])]
 final class MemberController extends AbstractController
 {
     #[Route('/', name: 'show')]
-    public function index(string $slug, UserRepository $userRepository, EntityManagerInterface $em): Response
+    public function index(string $uuid32, UserRepository $userRepository, EntityManagerInterface $em): Response
     {
-        $user = $userRepository->getUserByUuid($slug);
+        $user = $userRepository->getUserByUuid($uuid32);
         $sql = 'SELECT * FROM users WHERE uuid = :uuid';
         $conn = $em->getConnection();
         $stmt = $conn->prepare($sql);
