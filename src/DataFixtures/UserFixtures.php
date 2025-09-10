5dd624b57d671cb7e2be09a1ee312f78d0ea9bb5
aeim <aeim@hotmail.fr>
2025-09-10 17:21:06 +0200

    Add fixtures

---
+++ src/DataFixtures/UserFixtures.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\User;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+use Faker\Factory;
+use Faker\Generator;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+
+class UserFixtures extends Fixture
+{
+    public const BATCH_SIZE = 10;
+    private Generator $faker;
+
+    public function __construct(
+        private readonly UserPasswordHasherInterface $passwordHasher
+    ) {
+        $this->faker = Factory::create('fr_FR');
+    }
+
+    // Add users
+    public function load(ObjectManager $manager): void
+    {
+
+        for ($i=0; $i < self::BATCH_SIZE; $i++) { 
+            $user = new User();
+            $user->setFirstName($this->faker->firstName())
+                ->setLastName($this->faker->lastName())
+                ->setUsername($this->faker->userName())
+                ->setPhone('+33 6 12 34 56 ' . sprintf('%02d', $i))
+                ->setEmail($this->faker->email())
+                ->setPassword($this->passwordHasher->hashPassword($user, '2]~4t.C6=pqN23'))
+                ->setBio($this->faker->paragraph());
+            $manager->persist($user);
+            $this->addReference('user_' . $i, $user);
+        }
+
+        $manager->flush();
+    }
+}
