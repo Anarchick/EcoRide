4b57a60f6a1f3f3e0cba65e4f8a2a2ef7b260366
aeim <aeim@hotmail.fr>
2025-09-14 22:58:33 +0200

    Heroku assets upload

---
+++ public/assets/app-PFKhlTv.js
@@ -0,0 +1,23 @@
+import './bootstrap.js';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap';
+import './styles/app.css';
+import './styles/home/home.css';
+import htmx from 'htmx.org';
+
+window.htmx = htmx;
+
+// Make HTMX work with Symfony\ux-turbo
+document.addEventListener("turbo:load", function() {
+    if (window.htmx) {
+        htmx.process(document.body);
+    }
+});
+
+/*
+document.addEventListener('htmx:afterSwap', (event) => {
+    if (window.htmx) {
+        htmx.process(document.body);
+    }
+});
+*/
