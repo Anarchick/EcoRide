103da49a9f48e1a7960c6143f0352122a58ebbcc
aeim <aeim@hotmail.fr>
2025-10-16 19:04:12 +0200

    Add bootstrap tooltips

--- assets/app.js
+++ assets/app.js
@@ -1,7 +1,7 @@
 import './bootstrap.js';
 import'./layout_guide.js';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap';
+import { Tooltip } from 'bootstrap';
 import './styles/app.css';
 import './scripts/travel-search-bar.js'
 import './styles/home/home.css';
@@ -23,3 +23,11 @@ document.addEventListener('htmx:afterSwap', (event) => {
     }
 });
 */
+
+// Initialize Bootstrap tooltips
+document.addEventListener('DOMContentLoaded', function() {
+    var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'));
+    var tooltipList = tooltipTriggerList.map(function(tooltipTriggerEl) {
+        return new Tooltip(tooltipTriggerEl);
+    });
+});
