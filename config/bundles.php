6df6e5aee8bed18756e0c05702e4f57dcad80fe7
aeim <aeim@hotmail.fr>
2025-10-19 11:02:43 +0200

    Add doctrine/mongodb-odm-bundle

--- config/bundles.php
+++ config/bundles.php
@@ -18,4 +18,5 @@ return [
     Stof\DoctrineExtensionsBundle\StofDoctrineExtensionsBundle::class => ['all' => true],
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
     SpecShaper\EncryptBundle\SpecShaperEncryptBundle::class => ['all' => true],
+    Doctrine\Bundle\MongoDBBundle\DoctrineMongoDBBundle::class => ['all' => true],
 ];
