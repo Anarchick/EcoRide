965e3b6c9d1a531ef0357d13b3a509613bcce62f
aeim <aeim@hotmail.fr>
2025-10-26 10:51:21 +0100

    Replace uuid32 by uuid

--- src/Controller/MemberController.php
+++ src/Controller/MemberController.php
@@ -7,15 +7,17 @@ use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Routing\Requirement\Requirement;
+
 // Faster regex UUID
-#[Route('/member/{uuid32}', name: 'app_member_', requirements: ['uuid32' => '[a-f0-9]{32}'])]
+#[Route('/member/{uuid}', name: 'app_member_', requirements: ['uuid' => Requirement::UID_RFC4122])]
 final class MemberController extends AbstractController
 {
     #[Route('/', name: 'show')]
-    public function index(string $uuid32, UserRepository $userRepository, EntityManagerInterface $em): Response
+    public function index(string $uuid, UserRepository $userRepository, EntityManagerInterface $em): Response
     {
         /** @var User $user */
-        $user = $userRepository->getByUuid($uuid32);
+        $user = $userRepository->getByUuid($uuid);
         $sql = 'SELECT * FROM users WHERE uuid = :uuid';
         $conn = $em->getConnection();
         $stmt = $conn->prepare($sql);
