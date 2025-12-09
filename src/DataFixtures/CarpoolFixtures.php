351d7a4cf56e2d6c8aacb456434d6297da7e4322
aeim <aeim@hotmail.fr>
2025-12-09 10:29:14 +0100

    Add TransactionFixtures as a dependency in CarpoolFixtures

--- src/DataFixtures/CarpoolFixtures.php
+++ src/DataFixtures/CarpoolFixtures.php
@@ -22,6 +22,7 @@ class CarpoolFixtures extends Fixture implements DependentFixtureInterface
     {
         return [
             TravelFixtures::class,
+            TransactionFixtures::class,
         ];
     }
 
